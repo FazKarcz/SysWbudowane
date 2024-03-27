572f962addf8d8c2ebdc575f5a47416a8ea1aae3
Jakub Karcz <92019651+FazKarcz@users.noreply.github.com>
2024-03-27 15:57:28 +0100

    Create cw2.c

---
+++ cw2.c
@@ -0,0 +1,43 @@
+// CONFIG2
+#pragma config POSCMOD = HS             // Primary Oscillator Select (HS Oscillator mode selected)
+#pragma config OSCIOFNC = OFF           // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as CLKO (FOSC/2))
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
+#pragma config FNOSC = PRIPLL           // Oscillator Select (Primary Oscillator with PLL module (HSPLL, ECPLL))
+#pragma config IESO = OFF               // Internal External Switch Over Mode (IESO mode (Two-Speed Start-up) disabled)
+
+// CONFIG1
+#pragma config WDTPS = PS32768          // Watchdog Timer Postscaler (1:32,768)
+#pragma config FWPSA = PR128            // WDT Prescaler (Prescaler ratio of 1:128)
+#pragma config WINDIS = ON              // Watchdog Timer Window (Standard Watchdog Timer enabled,(Windowed-mode is disabled))
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (Watchdog Timer is disabled)
+#pragma config ICS = PGx2               // Comm Channel Select (Emulator/debugger uses EMUC2/EMUD2)
+#pragma config GWRP = OFF               // General Code Segment Write Protect (Writes to program memory are allowed)
+#pragma config GCP = OFF                // General Code Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG port is disabled)
+
+
+#include "xc.h"
+#include "libpic30.h"
+
+
+
+int main(void) {
+    unsigned portValue = 0;
+    TRISA = 0X0000;
+    TRISB = 0XFFFF;
+    
+    AD1CON1 = 0X80E4;
+    AD1CON2 = 0X0404;
+    AD1CON3 = 0X0F00;
+    AD1CHS = 0;
+    AD1CSSL = 0X0020;
+    
+    while(1){
+        while(!AD1CON1bits.DONE);
+        portValue = ADC1BUF0;
+        LATA = portValue;
+    }
+    
+    return 0;
+}
+
