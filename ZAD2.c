5baec5de5a38036384776abed1af5063b8e5a5bf
Jakub Karcz <92019651+FazKarcz@users.noreply.github.com>
2024-06-12 16:10:38 +0200

    Update ZAD2.c

--- ZAD2.c
+++ ZAD2.c
@@ -18,43 +18,44 @@
 #pragma config GCP = OFF                // General Code Segment Code Protect (Code protection is disabled)
 #pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG port is disabled)
 
-#include "xc.h"
+#include "xc.h" 
 
 int main(void) {
-    TRISA = 0x0000;
-    TRISB = 0xFFFF;
+    TRISA = 0x0000;  
+    TRISB = 0xFFFF; 
     
-    AD1CON1 = 0x80E4;
-    AD1CON2 = 0x0404;
-    AD1CON3 = 0x0F00;
-    AD1CHS = 0x00;
-    AD1CSSL = 0X20;
-   
-    unsigned int licznik = 0, portValue = 0;
-    char currentS6 = 0, prevS6 = 0;
-    
-    while(1){
-        while(!AD1CON1bits.DONE);
-        prevS6 = PORTDbits.RD6;
-        __delay32(1500000);
-        currentS6 = PORTDbits.RD6;
-        portValue = ADC1BUF0;
-        portValue = portValue>>2;
-        if(portValue >= 127){ //255 podzielone na pol
-            licznik++;
-            __delay32(10000000);
-            LATA = 1;
-            __delay32(10000000);
-            LATA = 0;
-            while(licznik >= 5 && PORTDbits.RD6){
-            LATA = 255;
+    AD1CON1 = 0x80E4;  
+    AD1CON2 = 0x0404;  
+    AD1CON3 = 0x0F00;  
+    AD1CHS = 0x00;     
+    AD1CSSL = 0X20;   
+
+    unsigned int licznik = 0, portValue = 0;  // Deklaracja zmiennych 
+    char currentS6 = 0, prevS6 = 0;           
+
+    while(1) {  // Pętla nieskończona
+        while(!AD1CON1bits.DONE);  // Oczekiwanie na zakończenie konwersji
+        prevS6 = PORTDbits.RD6;    // Zapisanie poprzedniego stanu przycisku RD6
+        __delay32(1500000);        // Opóźnienie
+        currentS6 = PORTDbits.RD6; // Odczyt aktualnego stanu przycisku RD6
+        portValue = ADC1BUF0;      // Odczyt wartości z bufora konwersji ADC1BUF0
+        portValue = portValue>>2;   // Przesunięcie bitowe w prawo o 2 bity (dzielenie przez 4)
+        
+        if(portValue >= 127){ // Jeśli wartość z bufora jest większa lub równa 127
+            licznik++;       // Inkrementacja licznika
+            __delay32(10000000);  // Opóźnienie
+            LATA = 1;             // Ustawienie wartości na porcie A na 1
+            __delay32(10000000);  // Opóźnienie
+            LATA = 0;             // Ustawienie wartości na porcie A na 0
+            while(licznik >= 5 && PORTDbits.RD6) {  // Pętla wykonuje się, gdy licznik jest większy lub równy 5 oraz przycisk RD6 jest wciśnięty
+                LATA = 255;  // Ustawienie wartości na porcie A na 255
             }
+        } else {
+            LATA = 0;  // W przeciwnym wypadku ustawienie wartości na porcie A na 0
+            licznik = 0;  // Zresetowanie licznika
         }
-        else{
-            LATA = 0;
-            licznik = 0;
-        }
-        __delay32(1000000);
+        __delay32(1000000);  // Opóźnienie
     }
-    return 0;
+    return 0;  
 }
+
